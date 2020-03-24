5a68f4152ed90cb007e969c0ba256e36f372a31f
Joaquin Raya <joaquin.raya@gmail.com>
2020-03-24 10:26:15 +0100

    First commit with basic project structure and first tests defined

---
+++ VendingMachine.Domain/VendingMachine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine.Domain
+{
+    public class VendingMachine
+    {
+        public void AcceptCoin(string coin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
